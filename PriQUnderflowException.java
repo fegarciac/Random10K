19a1f10df595976dfe61ecc36d3edfd53a26b3a2
Fernanda Garcia Camargo <60704470+fegarciac@users.noreply.github.com>
2021-11-11 16:57:17 -0500

    Create PriQUnderflowException.java

---
+++ PriQUnderflowException.java
@@ -0,0 +1,11 @@
+class PriQUnderflowException extends RuntimeException
+{
+  public PriQUnderflowException()
+  {
+    super();
+  }
+  public PriQUnderflowException(String message)
+  {
+    super(message);
+  }
+}
